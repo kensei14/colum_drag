2c774fe7b5d524ef05c4f1c2512fd220e4dfc0d0
kensei.yarimizu <kensei.yarimizu@10xlab.jp>
2012-07-26 17:24:03 +0900

    change insert processs to capseled one

--- column_drag.js
+++ column_drag.js
@@ -1,14 +1,73 @@
 /**
  * @author 鎗水謙星
  */
+var column_list = new Array(5);
+
+column_list.test = function() {
+	alert(column_list[0]);
+}
+
+column_list.exchange = function(i, j) {
+	var tmp;
+	/*
+	if
+	tmp = column_list[i];
+	column_list[i] = column_list[j];
+	column_list[j] = tmp;
+	*/
+}
+
+//moving_idをもつ列をtarget_idの前に、挿入します。
+column_list.insert = function(target_id, moving_id) {
+	var target_num, moving_num;
+	var moving_div;
+	
+	console.log("確認:    " + target_id +  "  " + moving_id);
+	console.log("for文前: " + target_num +  "  " + moving_num);
+	for(var i=0; i < column_list.length; i++) {
+		if(column_list[i].id == target_id) target_num = i;
+		if(column_list[i].id == moving_id) moving_num = i;
+	}
+	console.log("for文後: " + target_num +  "  " + moving_num);
+	
+	var col = column_list[moving_num];
+	
+	column_list.splice(moving_num, 1);
+	column_list.consoleOut();
+	//console.log("column" + moving_num + "  is deleted. ");
+	column_list.splice(target_num, 0, col);
+	column_list.consoleOut();
+	//console.log("target" + target_num + "  is deleted? ");
+	//alert(column_list.join());
+}
+
+column_list.show = function() {
+	var c_list = document.getElementById("column_list");
+	c_list.innerHTML = "";
+		
+	for(var i=0; i < column_list.length; i++) {
+		console.log(c_list.appendChild(column_list[i]));
+		console.log(column_list[i].id);
+	}
+}
+
+column_list.consoleOut = function() {
+	var out = "Console Oout   : ";
+	for(var i=0; i < column_list.length; i++) {
+		out += column_list[i].id + "  ";
+	};
+	console.log(out);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
 	for(var i=0; i<5; i++) {
-		createColumn(i);
+		column_list[i] = createColumn(i);
 	}
 }, false);
 
 
 function createColumn(i) {
+	var column_elem = document.getElementById("column_list");
 	var new_column = document.createElement("div");
 	var new_blank = document.createElement("div");
 	var new_dragzone = document.createElement("div");
@@ -16,14 +75,18 @@ function createColumn(i) {
 
 	new_column.appendChild(new_dragzone);
 	new_column.appendChild(new_content);
+	
+	column_elem.appendChild(new_blank);
+	column_elem.appendChild(new_column);
+	/*
 	document.body.appendChild(new_blank);
 	document.body.appendChild(new_column);
-	
+	*/
 	new_column.classList.add("column-size");
 	new_column.classList.add("column-decoration");
 	new_column.setAttribute("id", "column" + i);
 	
-	new_dragzone.className = "dragzone";
+	new_dragzone.classList.add("dragzone");
 	new_dragzone.setAttribute("draggable", "true");
 	new_dragzone.textContent = "title" + i;
 		
@@ -32,38 +95,42 @@ function createColumn(i) {
 
 	new_blank.classList.add("column-size");
 	new_blank.classList.add("blank");
-	new_blank.setAttribute("dropzone", "move");
 	new_blank.setAttribute("id", "blank" + i);
 
-	new_dragzone.ondragstart = function(event) {
+	new_dragzone.addEventListener("dragstart", function(event) {
 		//event.dataTransfer.addElement(this.parentNode);
 		event.dataTransfer.setData("Text", this.parentNode.getAttribute('id'));		//移動対象列要素のidをイベントオブジェクトに格納する。
 		return true;
-	}
+	}, false);
 
-	new_blank.ondrop = function(event) {
-		event.preventDefault();
-		
-		this.classList.add("blank");
-		insertDOM(this.id, event.dataTransfer.getData("Text"));	//列要素の並びを変更する。
-	}
-	
-	new_blank.ondragover = function(event){
-		event.preventDefault();		//デフォルトの動作を取り除く
-	}
-	
 	//空白要素の幅を変更する。(Blankクラスを挿入する)
-	new_blank.ondragenter = function(event) {
+	new_blank.addEventListener("dragenter", function(event) {
 		//列要素の直前・直後の空白要素でなければ
 		if((this.nextSibling.id != event.dataTransfer.getData("Text")) && (this.previousSibling.id != event.dataTransfer.getData("Text"))) {
 			this.classList.remove("blank");	
 		}
-	}
-	
+	}, false);
+		
 	//空白要素を元に戻す(Blankクラスを取り除く)
-	new_blank.ondragleave = function(event) {
+	new_blank.addEventListener("dragleave", function(event) {
 		this.classList.add("blank");
-	}
+	}, false);
+
+	new_blank.addEventListener("dragover", function(event) {
+		event.preventDefault();		//デフォルトの動作を取り除く
+	}, false);
+	
+	new_blank.addEventListener("drop", function(event) {
+		event.preventDefault();
+		
+		this.classList.add("blank");
+		column_list.insert(this.nextSibling.id, event.dataTransfer.getData("Text"));
+		column_list.show();
+		
+		//insertDOM(this.id, event.dataTransfer.getData("Text"));	//列要素の並びを変更する。
+	}, false);
+	
+	return new_column;
 }
 
 function insertDOM (target, moving) {
--- drag&drop.md
+++ drag&drop.md
@@ -1,20 +1,40 @@
-# Drag and Drop について
-
 ## Drag&Dropの基本
 * draggable要素をtrueにする。
-* element.setAttribute("draggable", "true");
+* drop先のdropzone属性を設定する。（"copy","move","link"）
 
-* event.dataTransfer.setDataをすると透過したものが表示される。
+## event.dataTransferオブジェクトについて
+
+* items,types,filesなどのプロパティがある。
+
+* setDataメソッドにより渡すデータをセットする。
+> event.dataTransfer.setData(fomart, data);
+> formatは“Text”など
+*** event.dataTransfer.setDataをすると透過したものが表示される。***
+*** addElementで透過表示する要素の指定 ***
 
 
-## DataTransferオブジェクトについて
+## ドラッグ＆ドロップ中のイベント
+* ondragstart() 	ドラッグ開始時
+* ondragover()		ドラッグ中
+* ondragenter()		ドラッグ中にある要素内に入った時
+* ondragleave()		ドラッグ中にある要素内を離れた時
+* ondrop()			ある要素にドロップした時
+ 
+*** ondragover / ondropイベントを実行する際には必ず両イベント内でevent.preventDefault()を行わなければならない！！！***
 
-## 絶対に必要な処理
-* ondragoverでは必ず event.preventDefault()を行わなければならない！！！
+(行わないとデフォルトの動作が実行され動かない。)
+
+
 * http://javascript-api.sophia-it.com/reference/%E3%83%89%E3%83%A9%E3%83%83%E3%82%B0%EF%BC%86%E3%83%89%E3%83%AD%E3%83%83%E3%83%97%E3%81%A7%E3%81%8D%E3%82%8B%E3%82%88%E3%81%86%E3%81%AB%E3%81%99%E3%82%8B%E3%81%AB%E3%81%AF
 
 ## デッキ移動インターフェースの実装
 
+### インターフェース仕様
+* デッキのドラッグによる列の配置変更
+* 移動元の透過表示 
+* 移動先の表示
+
+### デッキ移動方法
 1. デッキを作成する。
 2. 各列要素の直前に空要素を挿入しておく。
 3. DragOverされたらCSSにより空白要素の幅を変更する。
